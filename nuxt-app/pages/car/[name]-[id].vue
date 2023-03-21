de5ab8d9935ee8160f63f08dfba4ecc002010c1c
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2023-03-21 16:44:54 -0300

    Get car info from id

--- nuxt-app/pages/car/[name]-[id].vue
+++ nuxt-app/pages/car/[name]-[id].vue
@@ -1,11 +1,18 @@
 <script setup>
 
 const route = useRoute();
+const { cars } = useCars();
 const { toTitleCase } = useUtilities();
 
 useHead({
     title: `${toTitleCase(route.params.name)}`
 });
+
+const car = computed(() => {
+    return cars.find(c => {
+        return c.id === parseInt(route.params.id);
+    })
+})
 
 definePageMeta({
     layout: 'carcustom'
@@ -13,12 +20,11 @@ definePageMeta({
 
 </script>
 
-
 <template>
     <div>
-        <CarDetailHero />
-        <CarDetailAttributes />
-        <CarDetailDescription />
+        <CarDetailHero :car="car"/>
+        <CarDetailAttributes :features="car.features"/>
+        <CarDetailDescription :description="car.description"/>
         <CarDetailContact />
     </div>
 </template>
