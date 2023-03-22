eb5019acf1112f67bb5b79ff45254afc56047d82
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2023-03-21 23:04:44 -0300

    favorite car

--- nuxt-app/layouts/carcustom.vue
+++ nuxt-app/layouts/carcustom.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Navbar />
-        <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+        <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/3">
             <slot/>
         </div>
     </div>
