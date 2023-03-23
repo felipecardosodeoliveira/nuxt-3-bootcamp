ac382be633511abd021a43ccf4e5739f8a8c6ba6
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2023-03-22 22:10:28 -0300

    persist data in localstorage

--- nuxt-app/components/Car/Card.vue
+++ nuxt-app/components/Car/Card.vue
@@ -4,19 +4,23 @@ import heartFilled from "@/assets/heartFilled.png";
 import heartOutline from "@/assets/heartOutline.png";
 
 const props = defineProps({
-    car: Object
+    car: Object,
+    favored: Boolean
 });
 
-const favored = useState(`favored-${props.car.id}`, () => {
-    return false
-});
-
+const emit = defineEmits(["favor"]);
 
 </script>
 
 <template>
     <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px] relative">
-        <img :src=" favored ? heartFilled : heartOutline" class="absolute w-7 right-5 top-2 z-30" alt="" @click="favored = !favored">
+        <img 
+            :src=" favored ? heartFilled : heartOutline"
+            class="absolute w-7 right-5 top-2 z-30"
+            alt=""
+            @click="emit('favor', car.id)"
+        >
+        
         <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
             <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
             <div class="p-4 flex flex-col">
