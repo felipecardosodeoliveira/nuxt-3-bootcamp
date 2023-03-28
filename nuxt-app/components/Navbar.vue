4f7a0537033565a069b90d9a8ffbf2a7a87a703e
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2023-03-27 23:00:57 -0300

    auth user with google and supabase

--- nuxt-app/components/Navbar.vue
+++ nuxt-app/components/Navbar.vue
@@ -1,5 +1,30 @@
+<script setup>
+    const user = useSupabaseUser();
+
+    const supabase = useSupabaseClient();
+
+    const signOut = async () => {
+      const { error } = await supabase.auth.signOut();
+
+      user.value = null;
+      
+      if (error) {
+        console.log(error);
+      }
+
+      navigateTo("/");
+
+    }
+</script>
+
+
 <template>
     <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
       <NuxtLink to="/" class="text-3xl font-mono">cartrader</NuxtLink>
+      <div v-if="user" class="flex">
+        <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+        <p class="cursor-pointer" @click="signOut">Logout</p>
+      </div>
+      <NuxtLink v-else to="/login" >Login</NuxtLink>
     </header>
 </template>
